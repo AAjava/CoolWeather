4029f130ffc81cbe4482ef94de75e65b3f7bef22
AAjava <iam740020470jun@qq.com>
2017-03-21 17:01:46 +0800

    便利全国省市

---
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -0,0 +1,70 @@
+package com.coolweather.android;
+
+import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.coolweather.android.db.City;
+import com.coolweather.android.db.County;
+import com.coolweather.android.db.Province;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/3/21.
+ */
+public class ChooseAreaFragment extends Fragment {
+    public static final int LEVEL_PROVINCE=0;
+    public static final int LEVEL_CITY=1;
+    public static final int LEVEL_COUNTY=2;
+    private ProgressDialog progressdialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private List<String> dataList=new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    /***
+     * 省列表
+     */
+    private List<Province> provinceList;
+    /***
+     * 市列表
+     */
+    private List<City> cityList;
+    /***
+     * 县列表
+     */
+    private List<County> countyList;
+    /***
+     * 选中的省份
+     */
+    private Province selectedProvice;
+    /***
+     * 选中的城市
+     */
+    private City selectedCity;
+    /**
+     * 当前选中的级别
+     */
+    private int currentLevel;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       View view=inflater.inflate(R.layout.choose_area,container,false);
+        titleText= (TextView) view.findViewById(R.id.title_text);
+        backButton= (Button) view.findViewById(R.id.back_btn);
+        listView= (ListView) view.findViewById(R.id.list_view);
+        adapter=new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,dataList);
+        return view;
+    }
+}
--- app/src/main/java/com/coolweather/android/util/HttpUtil.java
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -1,10 +1,17 @@
 package com.coolweather.android.util;
 
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Created by Administrator on 2017/3/21.
  */
 public class HttpUtil {
     public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
-
+        OkHttpClient okHttpClient=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        Call call=okHttpClient.newCall(request);
+        call.enqueue(callback);
     }
 }
