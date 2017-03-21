540325047bb239699d2ed6696fd2ffdf9745a3bb
AAjava <iam740020470jun@qq.com>
2017-03-21 15:27:06 +0800

    配置3

---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,10 @@
+package com.coolweather.android.util;
+
+/**
+ * Created by Administrator on 2017/3/21.
+ */
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+
+    }
+}
